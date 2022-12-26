e678e91fb314d7869830c589b759b52eefdc7a82
DrGeebWalker <114679533+DrGeebWalker@users.noreply.github.com>
2022-12-26 17:27:41 -0600

    Code Clean Up
    Code is slightly cleaner. Preparing to try camera movement.

--- project 1/createHippoCampus.js
+++ project 1/createHippoCampus.js
@@ -2,10 +2,10 @@ var VSHADER_SOURCE =
 `uniform mat4 u_ModelMatrix;
  attribute vec4 a_Position;
  attribute vec4 a_Color;
+ uniform mat4 u_ProjMatrix;
  varying vec4 v_Color;
  void main() {
    gl_Position = u_ModelMatrix * a_Position;
-   gl_PointSize = 10.0;
    v_Color = a_Color;
  }`;	
 
@@ -29,91 +29,15 @@ var VSHADER_SOURCE =
 var canvas;	
 var gl;
 var modelMatrix = new Matrix4();;
+
+//projection matrix
+//ortho or projection view
+var projMatrix = new Matrix4();
+
+
 var g_ModelMatrixLoc;
 var floatsPerVertex = 7;
 var g_lastMS = Date.now();
-
-
-
-//head movement
-//up and down
-var hm_brake = 1.0;
-var hm_angle = 0.0;
-var hm_anglerate = -11.0;       // init Rotation angle rate, in degrees/second.
-var hm_min  = -20.0;       // init min, max allowed angle, in degrees.
-var hm_max  =  5.0;
-
-
-//up and down
-var hs_brake = 1.0;
-var hs_angle = 0.0;
-var hs_anglerate = -70.0;       
-var hs_min  = -10.0;       
-var hs_max  =  60.0;
-//side to side
-
-//end of head movement 
-
-
-//arm movement
-//shoulder movement
-var sh_anglerate = -10.0; 
-var el_anglerate = -15.0;    
-var hoof_anglerate = -20.0; 
-
-
-
-var arm_brake = 1.0;
-var sh_angle = 0.0; 
-
-// init Rotation angle rate, in degrees/second.
-var sh_min  = -10.0;       // init min, max allowed angle, in degrees.
-var sh_max  =  10.0;
-
-//end from shoulder
-
-//from elbow movement
-var el_angle = 0.0;     
-var el_min  = -15.0;    
-var el_max  =  5.0;
-
-//end from elbow
-
-//hoof movement
-var hoof_angle = 0.0;
-var hoof_min  = -90.0;    
-var hoof_max  =  5.0;
-
-//end of arms
-
-
-//tail
-var t_brake = 1.0;
-var t_angle = 0.0;
-var t_anglerate = -10.0;       // init Rotation angle rate, in degrees/second.
-var t_min  = -20.0;       // init min, max allowed angle, in degrees.
-var t_max  =  5.0;
-
-
-var t2_angle = 0.0;
-var t2_anglerate = -10.0;       // init Rotation angle rate, in degrees/second.
-var t2_min  = -2.0;       // init min, max allowed angle, in degrees.
-var t2_max  =  50.0;
-
-
-
-var t3_angle = 0.0;
-var t3_anglerate = -11.0;       // init Rotation angle rate, in degrees/second.
-var t3_min  = -50.0;       // init min, max allowed angle, in degrees.
-var t3_max  =  60.0;
-
-//fin
-var f_brake = 1.0;
-var f_angle = 0.0;
-var f_anglerate = -250.0;       // init Rotation angle rate, in degrees/second.
-var f_min  = -50.0;       // init min, max allowed angle, in degrees.
-var f_max  =  180.0;
-
 
 
 //end of tail movement
@@ -463,128 +387,110 @@ function animate() {
     animateFin(elapsedMS);
     animateHead(elapsedMS);
     animateHeadShake(elapsedMS);
+    
 }
+function changeAngleRate(x_angle, x_anglerate, x_min, x_max){
+    if((x_angle >= x_max && x_anglerate > 0) || // going over max, or
+    (x_angle <= x_min && x_anglerate < 0)  ) // going under min ?
+        return -1;	// YES: reverse direction.
 
-function animateHead(elapsedMS){
-
-    hm_angle += hm_anglerate * hm_brake * (elapsedMS * 0.001);	// update.
-
-    if((hm_angle >= hm_max && hm_anglerate > 0) || // going over max, or
-    (hm_angle <= hm_min && hm_anglerate < 0)  ) // going under min ?
-        hm_anglerate *= -1;	// YES: reverse direction.
-
-    if(hm_min > hm_max){// if min and max don't limit the angle, then
-        if(hm_angle< -180.0) hm_angle += 360.0;	// go to >= -180.0 or
-        else if(hm_angle >  180.0) hm_angle -= 360.0;	// go to <= +180.0
+    return 1;
+}
+function changeAngle(x_angle, x_min, x_max){
+    if(x_min > x_max){// if min and max don't limit the angle, then
+        if(x_angle< -180.0) return 360.0;	// go to >= -180.0 or
+        else if(x_angle >  180.0) return -360.0;	// go to <= +180.0
     }
+    return 0;
 
 }
 
+//head moves up and down 
+var hm_brake = 1.0;
+var hm_angle = 0.0;
+var hm_anglerate = -11.0; 
+function animateHead(elapsedMS){
+    hm_angle += hm_anglerate * hm_brake * (elapsedMS * 0.001);	// update.
+    hm_anglerate *= changeAngleRate(hm_angle, hm_anglerate, -20.0, 5.0);
+    hm_angle += changeAngle(hm_angle, -20.0, 5.0);
+}
+//head shakes when mouse is near
+var hs_brake = 1.0;
+var hs_angle = 0.0;
+var hs_anglerate = -70.0;   
 function animateHeadShake(elapsedMS){
     hs_angle += hs_anglerate * hs_brake * (elapsedMS * 0.001);	// update.
-
-    if((hs_angle >= hs_max && hs_anglerate > 0) || // going over max, or
-    (hs_angle <= hs_min && hs_anglerate < 0)  ) // going under min ?
-        hs_anglerate *= -1;	// YES: reverse direction.
-
-    if(hs_min > hs_max){// if min and max don't limit the angle, then
-        if(hs_angle< -180.0) hs_angle += 360.0;	// go to >= -180.0 or
-        else if(hs_angle >  180.0) hs_angle -= 360.0;	// go to <= +180.0
-    }
-
+    hs_anglerate *= changeAngleRate(hs_angle, hs_anglerate, -10.0, 60.0);
+    hs_angle += changeAngle(hs_angle, -10.0, 60.0);
 }
 
-
-
+//arms move around
+var sh_anglerate = -10.0; 
+var arm_brake = 1.0;
+var sh_angle = 0.0; 
+//elbow movement
+var el_angle = 0.0;    
+var el_anglerate = -15.0;    
+//hoof movement
+var hoof_angle = 0.0;
+var hoof_anglerate = -20.0; 
+//end of arms
 function animateHooves(elapsedMS){
     
     sh_angle += sh_anglerate * arm_brake * (elapsedMS * 0.001);	// update.
-
-    if((sh_angle >= sh_max && sh_anglerate > 0) || // going over max, or
-    (sh_angle <= sh_min && sh_anglerate < 0)  ) // going under min ?
-        sh_anglerate *= -1;	// YES: reverse direction.
-
-    if(sh_min > sh_max){// if min and max don't limit the angle, then
-        if(     sh_angle< -180.0) sh_angle += 360.0;	// go to >= -180.0 or
-        else if(sh_angle >  180.0) sh_angle -= 360.0;	// go to <= +180.0
-    }
+    sh_anglerate *= changeAngleRate(sh_angle, sh_anglerate, -10.0, 10.0);
+    sh_angle += changeAngle(hs_angle, -10.0, 10.0);
 
     //elbow movement
     el_angle += el_anglerate * arm_brake * (elapsedMS * 0.001);	// update.
+    el_anglerate *= changeAngleRate(el_angle, el_anglerate, -15.0, 5.0);
+    el_angle += changeAngle(el_angle, -15.0, 5.0);
 
-    if((el_angle >= el_max && el_anglerate > 0) || // going over max, or
-    (el_angle <= sh_min && el_anglerate < 0)  ) // going under min ?
-        el_anglerate *= -1;	// YES: reverse direction.
-
-    if(el_min > el_max){// if min and max don't limit the angle, then
-        if(     el_angle< -180.0) el_angle += 360.0;	// go to >= -180.0 or
-        else if(el_angle >  180.0) el_angle -= 360.0;	// go to <= +180.0
-    }
 
     //hoof movement
     hoof_angle += hoof_anglerate * arm_brake * (elapsedMS * 0.001);	// update.
+    hoof_anglerate *= changeAngleRate(hoof_angle, hoof_anglerate, -90.0, 5.0);
+    hoof_angle += changeAngle(hoof_angle, -90.0, 5.0);
 
-    if((hoof_angle >= hoof_max && hoof_anglerate > 0) || // going over max, or
-    (hoof_angle <= hoof_min && hoof_anglerate < 0)  ) // going under min ?
-        hoof_anglerate *= -1;	// YES: reverse direction.
-
-    if(hoof_min > hoof_max){// if min and max don't limit the angle, then
-        if(     hoof_angle< -180.0) hoof_angle += 360.0;	// go to >= -180.0 or
-        else if(hoof_angle >  180.0) hoof_angle -= 360.0;	// go to <= +180.0
-    }
 }
+
+//tail
+var t_brake = 1.0;
+var t_angle = 0.0;
+var t_anglerate = -10.0;    
+
+var t2_angle = 0.0;
+var t2_anglerate = -10.0;    
+
+var t3_angle = 0.0;
+var t3_anglerate = -11.0;   
 
 function animateTail(elapsedMS){
 
     t_angle += t_anglerate * t_brake * (elapsedMS * 0.001);	// update.
-
-    if((t_angle >= t_max && t_anglerate > 0) || // going over max, or
-    (t_angle <= t_min && t_anglerate < 0)  ) // going under min ?
-        t_anglerate *= -1;	// YES: reverse direction.
-
-    if(t_min > t_max){// if min and max don't limit the angle, then
-        if(     t_angle< -180.0) t_angle += 360.0;	// go to >= -180.0 or
-        else if(t_angle >  180.0) t_angle -= 360.0;	// go to <= +180.0
-    }
-
+    t_anglerate *= changeAngleRate(t_angle, t_anglerate, -20.0, 5.0);
+    t_angle += changeAngle(t_angle, -20.0, 5.0);
 
     t2_angle += t2_anglerate * t_brake * (elapsedMS * 0.001);	// update.
-
-    if((t2_angle >= t2_max && t2_anglerate > 0) || // going over max, or
-    (t2_angle <= t2_min && t2_anglerate < 0)  ) // going under min ?
-        t2_anglerate *= -1;	// YES: reverse direction.
-
-    if(t2_min > t2_max){// if min and max don't limit the angle, then
-        if(     t2_angle< -180.0) t2_angle += 360.0;	// go to >= -180.0 or
-        else if(t2_angle >  180.0) t2_angle -= 360.0;	// go to <= +180.0
-    }
-
-
+    t2_anglerate *= changeAngleRate(t2_angle, t2_anglerate, -2.0, 50.0);
+    t2_angle += changeAngle(t2_angle, -2.0, 50.0);
 
     t3_angle += t3_anglerate * t_brake * (elapsedMS * 0.001);	// update.
+    t3_anglerate *= changeAngleRate(t3_angle, t3_anglerate, -50.0, 60.0);
+    t3_angle += changeAngle(t3_angle, -50.0, 60.0);
 
-    if((t3_angle >= t3_max && t3_anglerate > 0) || // going over max, or
-    (t3_angle <= t3_min && t3_anglerate < 0)  ) // going under min ?
-        t3_anglerate *= -1;	// YES: reverse direction.
-
-    if(t3_min > t3_max){// if min and max don't limit the angle, then
-        if(     t3_angle< -180.0) t3_angle += 360.0;	// go to >= -180.0 or
-        else if(t3_angle >  180.0) t3_angle -= 360.0;	// go to <= +180.0
-    }
 
 }
 
+//fin slaps around when mouse is near tail
+var f_brake = 1.0;
+var f_angle = 0.0;
+var f_anglerate = -250.0;
 function animateFin(elapsedMS){
     f_angle += f_anglerate * f_brake * (elapsedMS * 0.001);	// update.
+    f_anglerate *= changeAngleRate(f_angle, f_anglerate, -50.0, 180.0);
+    f_angle += changeAngle(f_angle, -50.0, 180.0);
 
-    if((f_angle >= f_max && f_anglerate > 0) || // going over max, or
-    (f_angle <= f_min && f_anglerate < 0)  ) // going under min ?
-        f_anglerate *= -1;	// YES: reverse direction.
-
-    if(f_min > f_max){// if min and max don't limit the angle, then
-        if(     f_angle< -180.0) f_angle += 360.0;	// go to >= -180.0 or
-        else if(f_angle >  180.0) f_angle -= 360.0;	// go to <= +180.0
-    }
 }
 //draws
 function drawHorseHead(){
@@ -679,11 +585,6 @@ function drawLeg(addTime){
 
 
 function drawTail(){
-
-    //modelMatrix.translate(.4, -.1, 0, 0);
-
-    //modelMatrix.rotate(0, 0, 1, 0);
-
     pushMatrix(modelMatrix);
 
     modelMatrix.translate(-.29, -.1,0, 0);
@@ -708,11 +609,6 @@ function drawTail(){
     gl.drawArrays(gl.TRIANGLE_STRIP, smooth7Start/floatsPerVertex, smooth0Verts.length*2/floatsPerVertex);
    
 
-    // makeSphere(13, 17, 12, 0.24, -0.36, 0);
-
-    
-
-
     modelMatrix.translate(.24, -.36,0,0);
     modelMatrix.rotate(t3_angle, 0, 0, 1);
     modelMatrix.translate(-.24, 0.36,0,0);
@@ -726,8 +622,7 @@ function drawTail(){
     gl.uniformMatrix4fv(g_ModelMatrixLoc, false, modelMatrix.elements);
     gl.drawArrays(gl.TRIANGLE_STRIP, smooth10Start/floatsPerVertex, smooth0Verts.length*2/floatsPerVertex);
 
-    
-    //modelMatrix.rotate(f_angle, 0, 1, 0);
+
     pushMatrix(modelMatrix);
 
     modelMatrix.translate(.3, -.05,.25,0);
